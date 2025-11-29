a3852d8d522b6f3b538eb5420e3077af90488578
Wakisaka Hayato <hayato.wakisaka@gmail.com>
2025-11-30 04:11:24 +0900

    ジョジョの奇妙な冒険、呪術廻戦のデータベースを作成

--- app5.js
+++ app5.js
@@ -32,13 +32,6 @@ let sawara = [
   {id:4, code:"mista"}
 ];
 
-let zahyo = [
-  {id:1, name:"mapleforest1", x:6200, y:60, z:5600, memo:"拠点から少し遠い、漁りかけ"},
-  {id:2, name:"cave1", x:6245, y:40, z:5567, memo:"廃坑に続いてる、毒蜘蛛スポナーは破壊済み"},
-  {id:3, name:"mansion1", x:712, y:60, z:25240, memo:"拠点から遠い、拠点近くネザー経由で行ける"},
-  {id:4, name:"mapleforest2", x:700, y:55, z:5400, memo:"拠点から一番近い、地下にサクラダイヤモンド採掘場"}
-];
-
 app.get("/", (req, res) => {
   res.render('landing', { title: "ホーム", links: [
     { url: "/keiyo", text: "京葉線1"},
@@ -46,7 +39,7 @@ app.get("/", (req, res) => {
     { url: "/janken2", text: "じゃんけん2"},
     { url: "/sawara", text: "sawara" },
     { url: "/zahyo", text: "Minecraft座標一覧"},
-    { url: "/jojo3", text: "ジョジョの奇妙な冒険 スターダストクルセイダース スタンド一覧"},
+    { url: "/stand", text: "ジョジョの奇妙な冒険 スターダストクルセイダース スタンド一覧"},
     { url: "/jujutsu", text: "呪術廻戦 登場人物一覧"},
   ]});
 });
@@ -125,7 +118,15 @@ app.post("/keiyo2/update/:number", (req, res) => {
   res.redirect('/keiyo2' );
 });
 
-// Minecraft zahyo
+// Minecraft座標データベース
+//内容
+let zahyo = [
+  {id:1, name:"mapleforest1", x:6200, y:60, z:5600, memo:"拠点から少し遠い、漁りかけ"},
+  {id:2, name:"cave1", x:6245, y:40, z:5567, memo:"廃坑に続いてる、毒蜘蛛スポナーは破壊済み"},
+  {id:3, name:"mansion1", x:712, y:60, z:25240, memo:"拠点から遠い、拠点近くネザー経由で行ける"},
+  {id:4, name:"mapleforest2", x:700, y:55, z:5400, memo:"拠点から一番近い、地下にサクラダイヤモンド採掘場"}
+];
+
 // 一覧
 app.get("/zahyo", (req, res) => {
   res.render('zahyo', {zahyo: zahyo} );
@@ -139,8 +140,7 @@ app.get("/zahyo/create", (req, res) => {
 // Read
 app.get("/zahyo/:number", (req, res) => {
   const number = req.params.number;
-  const detail = zahyo[ number ];
-  res.render('zahyo_detail', {zahyo: detail, id: number} );
+  res.render('zahyo_detail', {zahyo: zahyo[ number ], id: number} );
 });
 
 // Delete
@@ -151,21 +151,22 @@ app.get("/zahyo/delete/:number", (req, res) => {
 
 // Create
 app.post("/zahyo", (req, res) => {
-  const id = zahyo.length + 1;
-  const name = req.body.name;
-  const x = req.body.x;
-  const y = req.body.y;
-  const z = req.body.z;
-  zahyo.push( { id: id, name: name, x: x, y: y, z: z } );
-  console.log( zahyo );
+  const id = zahyo.length > 0 ? zahyo[zahyo.length - 1].id + 1 : 1;
+  const newdata = {
+    name: req.body.name,
+    x: req.body.x,
+    y: req.body.y,
+    z: req.body.z
+  };
+  zahyo.push( newdata );
+  console.log( newdata );
   res.render('zahyo', {zahyo: zahyo} );
 });
 
 // Edit
 app.get("/zahyo/edit/:number", (req, res) => {
   const number = req.params.number;
-  const detail = zahyo[ number ];
-  res.render('zahyo_edit', {id: number, zahyo: detail} );
+  res.render('zahyo_edit', {id: number, zahyo: zahyo[number]} );
 });
 
 // Update
@@ -178,6 +179,223 @@ app.post("/zahyo/update/:number", (req, res) => {
   zahyo[req.params.number].memo = req.body.memo;
   console.log( zahyo );
   res.redirect('/zahyo' );
+});
+
+// 2. ジョジョの奇妙な冒険 スタンドデータベース
+
+let stand = [
+  {
+    id: 1, 
+    name: "スタープラチナ", 
+    name2: "空条承太郎", 
+    dpower: "A", 
+    speed: "A", 
+    range: "C", 
+    persistance: "A", 
+    precision: "A", 
+    dpotential: "A" 
+  },
+  {
+    id: 2, 
+    name: "マジシャンズ・レッド", 
+    name2: "モハメド・アヴドゥル", 
+    dpower: "B", 
+    speed: "B", 
+    range: "C", 
+    persistance: "B", 
+    precision: "C", 
+    dpotential: "D" 
+  },
+  {
+    id: 3, 
+    name: "ハイエロファントグリーン", 
+    name2: "花京院典明", 
+    dpower: "C", 
+    speed: "B", 
+    range: "A", 
+    persistance: "B", 
+    precision: "C", 
+    dpotential: "D" 
+  },
+  {
+    id: 4, 
+    name: "シルバーチャリオッツ", 
+    name2: "J・P・ポルナレフ", 
+    dpower: "C", 
+    speed: "A", 
+    range: "C", 
+    persistance: "B", 
+    precision: "B", 
+    dpotential: "C" 
+  },
+  {
+    id: 5, 
+    name: "ザ・ワールド", 
+    name2: "DIO", 
+    dpower: "A", 
+    speed: "A", 
+    range: "C", 
+    persistance: "A", 
+    precision: "B", 
+    dpotential: "B" 
+  }
+];
+
+// 一覧
+app.get("/stand", (req, res) => {
+  res.render('stand', { stand: stand });
+});
+
+// Create
+app.get("/stand/create", (req, res) => {
+  res.redirect('/public/stand_add.html');
+});
+
+// Read
+app.get("/stand/:number", (req, res) => {
+  const number = req.params.number;
+  res.render('stand_detail', { stand: stand[ number ], id: number });
+});
+
+// Delete
+app.get("/stand/delete/:number", (req, res) => {
+  stand.splice(req.params.number, 1);
+  res.redirect('/stand' );
+});
+
+// Create
+app.post("/stand", (req, res) => {
+  const id = stand.length > 0 ? stand[stand.length - 1].id + 1 : 1;
+  const newdata = {
+    id: id,
+    name: req.body.name,
+    name2: req.body.name2,
+    dpower: req.body.dpower,
+    speed: req.body.speed,
+    range: req.body.range,
+    persistance: req.body.persistance,
+    precision: req.body.precision,
+    dpotential: req.body.dpotential
+  };
+  stand.push(newdata);
+  console.log( newdata );
+  res.render('stand', { stand: stand });
+});
+
+// Edit
+app.get("/stand/edit/:number", (req, res) => {
+  const number = req.params.number;
+  res.render('stand_edit', { id: number, stand: stand[number] });
+  });
+
+// Update
+app.post("/stand/update/:number", (req, res) => {
+  stand[req.params.number].id = req.body.id;
+  stand[req.params.number].name = req.body.name;
+  stand[req.params.number].name2 = req.body.name2;
+  stand[req.params.number].dpower = req.body.dpower;
+  stand[req.params.number].speed = req.body.speed;
+  stand[req.params.number].range = req.body.range;
+  stand[req.params.number].persistance = req.body.persistance;
+  stand[req.params.number].precision = req.body.precision;
+  stand[req.params.number].dpotential = req.body.dpotential;
+  console.log( stand );
+  res.redirect('/stand');
+});
+
+
+// 呪術廻戦 登場人物データベース
+
+let jujutsu = [
+  { 
+    id: 1, 
+    name: "虎杖悠仁", 
+    rank: "なし", 
+    jutsushiki: "なし", 
+    ryoiki: "なし" 
+  },
+  { 
+    id: 2, 
+    name: "伏黒恵", 
+    rank: "2級", 
+    jutsushiki: "十種影法術", 
+    ryoiki: "嵌合暗翳庭" 
+  },
+  { 
+    id: 3, 
+    name: "釘崎野薔薇", 
+    rank: "3級", 
+    jutsushiki: "芻霊呪法", 
+    ryoiki: "なし" 
+  },
+  { 
+    id: 4, 
+    name: "五条悟", 
+    rank: "特級", 
+    jutsushiki: "無下限呪術", 
+    ryoiki: "無量空処" 
+  },
+  { 
+    id: 5, 
+    name: "両面宿儺", 
+    rank: "特級", 
+    jutsushiki: "御厨子", 
+    ryoiki: "伏魔御廚子" 
+  }
+];
+
+// 一覧
+app.get("/jujutsu", (req, res) => {
+  res.render('jujutsu', { jujutsu: jujutsu });
+});
+
+// Create
+app.get("/jujutsu/create", (req, res) => {
+  res.redirect('/public/jujutsu_add.html');
+});
+
+// Read
+app.get("/jujutsu/:number", (req, res) => {
+  const number = req.params.number;
+  res.render('jujutsu_detail', { jujutsu: jujutsu[number], id: number });
+});
+
+// Delete
+app.get("/jujutsu/delete/:number", (req, res) => {
+  jujutsu.splice(req.params.number, 1);
+  res.redirect('/jujutsu');
+});
+
+// Create
+app.post("/jujutsu", (req, res) => {
+  const id = jujutsu.length > 0 ? jujutsu[jujutsu.length - 1].id + 1 : 1;
+  const newdata = {
+    id: id,
+    name: req.body.name,
+    rank: req.body.rank,
+    jutsushiki: req.body.jutsushiki,
+    ryoiki: req.body.ryoiki
+  };
+  jujutsu.push(newdata);
+  console.log( newdata );
+  res.render('jujutsu', { jujutsu: jujutsu });
+});
+
+// Edit
+app.get("/jujutsu/edit/:number", (req, res) => {
+  const number = req.params.number;
+  res.render('jujutsu_edit', { id: number, jujutsu: jujutsu[number] });
+});
+
+// Update
+app.post("/jujutsu/update/:number", (req, res) => {
+  jujutsu[req.params.number].id = req.body.id;
+  jujutsu[req.params.number].name = req.body.name;
+  jujutsu[req.params.number].rank = req.body.rank;
+  jujutsu[req.params.number].jutsushiki = req.body.jutsushiki;
+  jujutsu[req.params.number].ryoiki = req.body.ryoiki;
+  console.log( jujutsu );
+  res.redirect('/jujutsu');
 });
 
 app.get("/keiyo_add2", (req, res) => {
